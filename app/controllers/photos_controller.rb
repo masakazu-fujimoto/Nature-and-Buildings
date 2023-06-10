49dcb5582c7145c44831d4bdde9acdb8647e2cb6
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-10 20:27:00 +0900

    createアクション定義

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,13 @@ class PhotosController < ApplicationController
   def new
     @photo = Photo.new
   end
+
+  def create
+    Photo.create(photo_params)
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:name, :image, :text)
+  end
 end
