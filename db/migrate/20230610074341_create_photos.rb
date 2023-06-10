314018a8715b79f148a7ad728ed4e040bb55ee19
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-10 16:50:45 +0900

    カラム追加

--- db/migrate/20230610074341_create_photos.rb
+++ db/migrate/20230610074341_create_photos.rb
@@ -1,7 +1,12 @@
 class CreatePhotos < ActiveRecord::Migration[6.0]
   def change
     create_table :photos do |t|
-
+      t.string :name
+      t.string :text
+      t.string :image
+      t.string :address
+      t.float :latitude
+      t.float :longitude
       t.timestamps
     end
   end
