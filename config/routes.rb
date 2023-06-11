b014cd6cf81b3da13c5b0abb99d7a238f5e6094f
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-11 23:19:22 +0900

    showアクションルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'photos#index'
-  resources :photos, only: [:index, :new, :create]
+  resources :photos, only: [:index, :new, :create, :show]
 end
