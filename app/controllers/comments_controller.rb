1d54bb6e516db2859006000d970d8b8026279dd5
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-12 21:06:02 +0900

    createアクション設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to photo_path(@comment.photo) 
+    else
+      @photo = @comment.photo
+      @comments = @photo.comments
+      render "photos/show" 
+    end
+  end
 end
