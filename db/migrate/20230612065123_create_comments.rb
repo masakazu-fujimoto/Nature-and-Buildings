3943d4c0f3ebb8619c4775877de047f48824141e
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-12 15:56:29 +0900

    参照外部キーの記述

--- db/migrate/20230612065123_create_comments.rb
+++ db/migrate/20230612065123_create_comments.rb
@@ -2,6 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text :content, null: false
+      t.references :photo, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
