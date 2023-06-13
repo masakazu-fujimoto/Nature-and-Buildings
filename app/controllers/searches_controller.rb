361b2e4675ebbe59d12762e9ba7d5bd024eaf66f
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-13 23:23:23 +0900

    ransackインストール

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,4 +8,15 @@ class SearchesController < ApplicationController
       @photos = Photo.looks(params[:search], params[:word])
     end
   end
+
+  def show
+    @photo = Photo.new
+      if @photo.save
+         redirect_to searches_path(@photo.id) #セーブできた時
+      else
+         @photos = Photo.all # この行を追加
+      end
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'photos#index'
-  resources :searches
+  resource :searches
   resources :photos do
     resources :comments, only: :create
   end
