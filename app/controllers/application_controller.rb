f5c17a92db2f415522fb7d53f9dbb186722910d9
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-13 23:25:34 +0900

    検索機能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
+
+  def set_search
+    @search = Photo.ransack(params[:q])
+    @search_photos = @search.result
+  end
 
   private
   def configure_permitted_parameters
