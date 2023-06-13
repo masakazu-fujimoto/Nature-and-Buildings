f3bf94b4f10a758ce780a0a5d07ad84569980e53
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-13 21:30:23 +0900

    検索の条件分岐

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,18 @@ class Photo < ApplicationRecord
   validates :name, presence: true
   validates :text, presence: true
   validates :image, presence: true
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @photo = Photo.where("name LIKE?","#{word}")
+    elsif search == "forward_match"
+      @photo = Photo.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @photo = Photo.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @photo = Photo.where("name LIKE?","%#{word}%")
+    else
+      @photo = Photo.all
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :photos
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("nickname LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("nickname LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("nickname LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("nickname LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 end
