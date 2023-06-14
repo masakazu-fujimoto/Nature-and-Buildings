b382c90277799a9cd91db0266d1a8e4841236146
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-14 23:51:45 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_q, only: [:index, :search]
+  before_action :set_q
   
   def search
     @results = @q.result
   end
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 
   def set_q
     @q = Photo.ransack(params[:q])
