511448ae97a985dcccc1e713b62f3d08d9533a53
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-14 19:02:53 +0900

    アソシエーション設定

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,22 +1,10 @@
 class Photo < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
   has_many :comments, dependent: :destroy
 
   validates :name, presence: true
   validates :text, presence: true
   validates :image, presence: true
 
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @photo = Photo.where("name LIKE?","#{word}")
-    elsif search == "forward_match"
-      @photo = Photo.where("name LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @photo = Photo.where("name LIKE?","%#{word}")
-    elsif search == "partial_match"
-      @photo = Photo.where("name LIKE?","%#{word}%")
-    else
-      @photo = Photo.all
-    end
-  end
 end
