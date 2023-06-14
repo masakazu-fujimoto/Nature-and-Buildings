993ee3669638edb2e0aff60b30104dbc7a2adc08
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-14 18:16:50 +0900

    search ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'photos#index'
-  resource :searches
   resources :photos do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :users do
+    collection do
+      get 'search'
+    end
   end
 end
