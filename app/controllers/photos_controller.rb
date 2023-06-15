dbbddc1879b0358bffbe618fdb7afa295334ce15
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-16 01:12:58 +0900

    User_idカラム追加

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @photos = Photo.all
@@ -40,7 +40,7 @@ class PhotosController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to new_user_session_path
+      redirect_to action: :index
     end
   end 
 end
