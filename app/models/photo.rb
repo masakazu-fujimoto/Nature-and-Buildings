da22c992182944710db3defd2c6921559441e282
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-06-21 18:23:49 +0900

    addressバリデーション

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,6 +6,7 @@ class Photo < ApplicationRecord
   validates :name, presence: true
   validates :text, presence: true
   validates :image, presence: true
+  validates :address, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
